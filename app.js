62f931f133b5636c1fac4d840672e8495fd44f13
chris <dwatow@gmail.com>
2019-05-22 11:21:53 +0800

    modify log infomate of backend

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ var router = express.Router();
 var multer  = require('multer')
 var upload = multer({ dist: 'uploads/' })
 
-app.listen(3333, () => console.log('用瀏覽器開啟 http://localhost:3333\n'));
+app.listen(3333, () => console.log('node is running at http://localhost:3333/. Press Ctrl+C to stop.\n'));
 
 app.use(express.static('public'));
-app.use('/static', express.static('files'));  // 提供下
+app.use('/static', express.static('files'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -16,13 +16,13 @@ app.use(bodyParser.json());
 
 // POST method route
 app.post('/upload', upload.single('myUploadFile'), (req, res) => {
-  const information = `收到上傳資訊:
-  文字欄位: ${req.body.filename}
+  const information = `informate of form:
+  field of text: ${req.body.filename}
 
-  上傳檔案
-  檔案欄位名稱: ${req.file.fieldname}
-  原始檔名: ${req.file.originalname}
-  檔案類型: ${req.file.mimetype}
+  upload file
+  field of file: ${req.file.fieldname}
+  file name: ${req.file.originalname}
+  file of MIME type: ${req.file.mimetype}
   `
   console.log(information);
   res.send({
